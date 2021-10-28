8ed92dd4c0c5ef57ebf75ddf1dc053afdf1deebf
Farzam Ahmad <farzam135@gmail.com>
2021-10-28 15:38:14 -0400

    created experiences table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_191432) do
+ActiveRecord::Schema.define(version: 2021_10_28_192206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,20 @@ ActiveRecord::Schema.define(version: 2021_10_28_191432) do
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "experiences", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.string "company", null: false
+    t.string "location"
+    t.boolean "current", null: false
+    t.date "start_date", null: false
+    t.date "end_date"
+    t.string "industry"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "likes", force: :cascade do |t|
