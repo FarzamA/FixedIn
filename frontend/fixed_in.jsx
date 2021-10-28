cdc4113423dc5c3025737938ba159735538c3b1b
Farzam Ahmad <farzam135@gmail.com>
2021-10-28 14:29:53 -0400

    created static page and route for static page

---
+++ frontend/fixed_in.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+
+    ReactDOM.render(<h1>Welcome to FixedIn</h1>, root);
+});
