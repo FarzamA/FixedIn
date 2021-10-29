e3f7e06b3b3c0230b11c630fce01a0117c5ef13e
Farzam Ahmad <farzam135@gmail.com>
2021-10-29 14:14:00 -0400

    fixed column names for posts and comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_195140) do
+ActiveRecord::Schema.define(version: 2021_10_29_181123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.text "body", null: false
-    t.integer "author_id", null: false
+    t.integer "user_id", null: false
     t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "connections", force: :cascade do |t|
@@ -75,11 +75,11 @@ ActiveRecord::Schema.define(version: 2021_10_28_195140) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body", null: false
-    t.integer "author_id", null: false
+    t.integer "user_id", null: false
     t.integer "like_count", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_posts_on_author_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
