5e8108c4ea0585b8e632b0c163e7f02881ea3533
Farzam Ahmad <farzam135@gmail.com>
2021-10-30 17:36:44 -0400

    created basic backend routes with api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
 
-  namespace :api, defaults: { format: :json } do 
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:index, :create, :show, :update]
+    resources :posts, except: [:edit, :new]
+    resources :comments, only: [:index, :create, :update, :destroy]
+    resources :likes, only: [:index, :create, :destroy]
+    resources :experiences, only: [:index, :create, :update, :destroy]
+    resources :connections, only: [:index, :create, :update, :destroy]
+    
+    resource :session, only: [:create, :destroy]
   end
 end
