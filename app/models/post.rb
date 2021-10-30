357988d63fdcb5e5579d4f50494e4e2e1ed8634c
Farzam Ahmad <farzam135@gmail.com>
2021-10-30 16:39:19 -0400

    fixed spelling of dependent in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,7 +14,7 @@ class Post < ApplicationRecord
 
     belongs_to :user
 
-    has_many :comments, depndent: :destroy
+    has_many :comments, dependent: :destroy
 
     has_many :likes, as: :likeable, dependent: :destroy
 
