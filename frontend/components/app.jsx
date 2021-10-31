05289edecb2e66db678968241fb5e1e8119e4789
Farzam Ahmad <farzam135@gmail.com>
2021-10-31 01:26:15 -0400

    working on login and sign up form containers

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,9 @@ import React from "react";
 const App = () => (
   <div>
     <h1>FixedIn</h1>
+
+    <AuthRoute path='/login' component={} />
+    <AuthRoute path='/signup' component={} />
   </div>
 );
 
