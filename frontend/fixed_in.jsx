ed0babab537d6f1778236f3831e5633aad64548d
Farzam Ahmad <farzam135@gmail.com>
2021-10-30 23:47:22 -0400

    created a root and app component

--- frontend/fixed_in.jsx
+++ frontend/fixed_in.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
+import configureStore from './store/store'
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    const store = configureStore();
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
 
-    ReactDOM.render(<h1>Welcome to FixedIn</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });
