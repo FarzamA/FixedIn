622b3e34bf6bee25be7c5cc5194affe2937a0253
Farzam Ahmad <farzam135@gmail.com>
2021-10-31 02:50:37 -0400

    added more actions to session actions in order to handle multiple sign up forms

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,9 +1,16 @@
 import * as SessionAPI from '../util/session_api_util'
 import { RECEIVE_SESSION_ERRORS } from '../reducers/session_errors_reducer';
+
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const RECEIVE_USER = 'RECEIVE_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 
+// in order to handle sign up actions
+export const RECEIVE_USER_EMAIL = 'RECEIVE_USER_EMAIL';
+export const RECEIVE_USER_NAME = 'RECEIVE_USER_NAME';
+export const RECEIVE_USER_LOCATION = 'RECEIVE_USER_LOCATION';
+export const RECEIVE_USER_JOB = 'RECEIVE_USER_JOB';
+export const RECEIVE_USER_STUDENT = 'RECEIVE_USER_STUDENT';
 
 export const receiveCurrentUser = payload => ({
     type: RECEIVE_CURRENT_USER,
@@ -18,6 +25,31 @@ export const receiveUser = payload => ({
     experiences: payload.experiences,
     educations: payload.education
 });
+
+export const receiveUserEmail = email => ({
+    type: RECEIVE_USER_EMAIL,
+    email
+});
+
+export const receiveUserName = name => ({
+    type: RECEIVE_USER_NAME,
+    name
+});
+
+export const receiveUserLocation = location => ({
+    type: RECEIVE_USER_LOCATION,
+    location
+});
+
+export const receiveUserJob = job => ({
+    type: RECEIVE_USER_JOB,
+    job
+});
+
+export const receiveUserStudent = student => ({
+    type: RECEIVE_USER_STUDENT,
+    student
+})
 
 export const logoutCurrentUser = () => ({
     type: LOGOUT_CURRENT_USER
--- frontend/components/session_form/email_form_container.jsx
+++ frontend/components/session_form/email_form_container.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { loginUser } from '../../util/session_api_util';
+import { receiveUserEmail, loginUser } from '../../actions/session_actions';
