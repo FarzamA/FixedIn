4122a0bc125c93aa6b17d4efbffb7331ad177252
Farzam Ahmad <farzam135@gmail.com>
2021-11-01 14:43:35 -0400

    reworked store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
 // in order to only have logger during development mode
-const middle = [thunk]
+const middlewares = [thunk];
+
 if (process.env.NODE_ENV !== "production") {
-    middle.push(logger);
-};
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) => (
     createStore(rootReducer, preloadedState, applyMiddleware(...middle))
