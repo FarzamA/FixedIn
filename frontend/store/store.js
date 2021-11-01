2716462aa1e4389ec2bb1adf1ce10fae7aa6ce3f
Farzam Ahmad <farzam135@gmail.com>
2021-11-01 14:44:07 -0400

    reworked store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const configureStore = (preloadedState = {}) => (
-    createStore(rootReducer, preloadedState, applyMiddleware(...middle))
+    createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
 );
 
 export default configureStore;
