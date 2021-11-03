6aa7e3aa67235390331d5b7753e0df2118f9268f
Farzam Ahmad <farzam135@gmail.com>
2021-11-02 21:56:51 -0400

    working on splash page styling

--- frontend/components/header/login_header.jsx
+++ frontend/components/header/login_header.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LoginHeader = () => (
-    <div className='nav-bar'>
+    <>
+        <nav className='session-nav'>
         <Link to='/'>
             <img src={window.logo} className='logo' />
         </Link>
-        <nav className='session-nav'>
             <Link to='/signup' className='splash-signup'>Join Now</Link>
             <Link to='/login' className='splash-signin'>Sign In</Link>
         </nav>
-    </div>
+    </>
 );
 
 export default LoginHeader;
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -7,7 +7,7 @@ const Splash = () => (
             <p className='splash-text'>Welcome to your professional community</p>
             <LoginFormContainer />
         </div>
-        <div>
+        <div className='splash-img-div'>
             <img src={window.splashImg} className='splash-img' />
             <p className='splash-credits'>Illustration by <a href="https://icons8.com/illustrations/author/603d1fd6123f9916a4db9ee6">  Irina Molchanova</a> from <a href="https://icons8.com/illustrations">Ouch!</a></p>
         </div>
