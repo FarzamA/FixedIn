5d218e0f34601485779db87002b402ac87ab2ef6
Farzam Ahmad <farzam135@gmail.com>
2021-11-03 12:13:15 -0400

    adjusting css for 404 page

--- frontend/components/not_found/not_found.jsx
+++ frontend/components/not_found/not_found.jsx
@@ -4,8 +4,13 @@ import { connect } from 'react-redux';
 class NotFound extends React.Component {
     render() {
         return (
-            <div>
+            <div className='explore'>
+                <h2>Page not found</h2>
+                <p>
+                    Uh oh, we can’t seem to find the page you’re looking for. Try going back to the previous page.
+                </p>
                 <img src={window.notFound}></img>
+                <div>Illustration by <a href="https://icons8.com/illustrations/author/603d1fd6123f9916a4db9ee6">  Irina Molchanova</a> from <a href="https://icons8.com/illustrations">Ouch!</a></div>
             </div>
         )
     }
