264b75452e9453185b7d0808bf3eac0153cf8e26
Farzam Ahmad <farzam135@gmail.com>
2021-11-08 17:28:33 -0500

    finished wiriting post actions

---
+++ frontend/actions/post_actions.js
@@ -0,0 +1,41 @@
+import * as PostAPI from '../util/post_api';
+
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+export const RECEIVE_POST = 'RECEIVE_POST';
+export const REMOVE_POST = 'REMOVE_POST';
+
+export const receivePosts = payload => ({
+    type: RECEIVE_POSTS,
+    posts: payload.posts,
+    users: payload.users
+});
+
+const receivePost = post => ({
+    type: RECEIVE_POST,
+    post
+});
+
+const removePost = postId => ({
+    type: REMOVE_POST,
+    postId
+});
+
+export const fetchPosts = (offset) => dispatch => (
+    PostAPI.fetchPosts(offset).then(posts => dispatch(receivePosts(posts)))
+);
+
+export const fetchPost = postId = dispatch => (
+    PostAPI.fetchPost(postId).then(post => dispatch(receivePost(post)))
+);
+
+export const createPost = formData => dispatch => (
+    PostAPI.createPost(formData).then(post => dispatch(receivePost(post)))
+);
+
+export const updatePost = post => dispatch => (
+    PostAPI.updatePost(post).then(post => dispatch(receivePost(post)))
+);
+
+export const deletePost = postId => dispatch => (
+    PostAPI.deletePost(postId).then(() => dispatch(removePost(postId)))
+);
