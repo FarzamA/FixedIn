b70702ac33aa4ea532741b566368274f00fcc8b9
Farzam Ahmad <farzam135@gmail.com>
2021-11-10 14:34:57 -0500

    added delay to search results

--- frontend/components/header/search_bar.jsx
+++ frontend/components/header/search_bar.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { searchUsers } from '../../util/session_api_util';
 
+//allows for a delay before search bar pops up
+let delay;
+const debounce = (callback, wait = 250) => {
+  return (...args) => {
+    clearTimeout(delay);
+    delay = setTimeout(() => { callback.apply(null, args); }, wait);
+  };
+};
+
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
@@ -16,9 +25,11 @@ class SearchBar extends React.Component {
 
     handleInput(e) {
          if (e.target.value.length) {
-             searchUsers(e.target.value).then(results => {
-                 this.setState({ results });
-             });
+            debounce(() => {
+                searchUsers(e.target.value).then(results => {
+                    this.setState({ results });
+                });
+            }, 300)();
          } else {
              this.setState({ results: [] });
          }
@@ -39,6 +50,11 @@ class SearchBar extends React.Component {
                     <ul className='search-results'>
                         {this.state.results.map(user => (
                             <li key={user.id} onClick={() => {this.redirectUser(user.id)}}>
+
+                                <div className='avatar-smaller'>
+                                    <img src={user.avatarUrl || window.defaultUser} alt="Avatar"/>
+                                </div>
+
                                 <p>{`${user.firstName} ${user.lastName}`}</p>
                             </li>
                         ))}
