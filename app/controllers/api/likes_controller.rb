c4b1500eb328fe4144048b9b2d5cbd647f1f989e
Farzam Ahmad <farzam135@gmail.com>
2021-12-02 15:45:05 -0500

    add likes controller

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -18,4 +18,41 @@ class Api::LikesController < ApplicationController
         
         render json: num_likes
     end
+
+    def user_liked 
+        @like = Like.find_by(
+          likeable_id: like_params[:likeable_id], 
+          likeable_type: like_params[:likeable_type],
+          user_id: like_params[:user_id]
+        )
+        
+        if @like
+          render :show
+        else
+          render json: 0
+        end
+    end
+
+    def create 
+        @like = Like.new(like_params)
+    
+        if @like.save 
+          render :show 
+        else
+          render json: @like.errors.full_messages
+        end
+    end
+    
+    def destroy 
+        @like = Like.find(params[:id])
+
+        @like.destroy 
+        render :show
+    end
+    
+    private 
+
+    def like_params
+        params.require(:like).permit(:user_id, :likeable_id, :likeable_type)
+    end
 end
